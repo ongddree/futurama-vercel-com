17f475ec27f30c77605a10715ba23a76b7a84250
seoyoung park <ongddree@gmail.com>
2021-12-27 14:39:37 +0900

    :recycle: ROUTES 추상화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,34 @@
 export const API_ENDPOINT = "https://api.sampleapis.com/futurama/";
+
+export const ROUTES = [
+  {
+    ID: "0",
+    PATH: "/info",
+    LABEL: "info",
+  },
+  {
+    ID: "1",
+    PATH: "/characters",
+    LABEL: "characters",
+  },
+  {
+    ID: "2",
+    PATH: "/cast",
+    LABEL: "cast",
+  },
+  {
+    ID: "3",
+    PATH: "/episodes",
+    LABEL: "episodes",
+  },
+  {
+    ID: "4",
+    PATH: "/qustions",
+    LABEL: "qustions",
+  },
+  {
+    ID: "5",
+    PATH: "/inventory",
+    LABEL: "inventory",
+  },
+];
