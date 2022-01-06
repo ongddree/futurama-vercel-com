105db69fd3581d7c6835b221f7969ab3c22c394c
seoyoung park <ongddree@gmail.com>
2022-01-07 01:40:53 +0900

    :lipstick: loading 스타일 및 assets 폴더 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,37 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { fa-cog fa-spin } from "@fortawesome/free-solid-svg-icons";
+import styled from "@emotion/styled";
 
 export const Loading = () => {
   return (
-    <div>
-      {/* <FontAwesomeIcon icon={fa-cog fa-spin} size="2x" spin={true} /> */}
-    </div>
+    <Container>
+      <Loadimg src="assets/loading-1.gif" alt="" />
+    </Container>
   );
 };
+
+const Loadimg = styled.img`
+  position: fixed;
+  display: block;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+`;
+
+const Container = styled.div`
+  border-radius: 40px;
+  &:after {
+    content: "";
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: radial-gradient(
+      circle,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(61, 61, 61, 1) 29%,
+      rgba(0, 0, 0, 1) 100%
+    );
+    opacity: 0.7;
+  }
+`;
