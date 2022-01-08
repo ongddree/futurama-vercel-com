a451d2b242e0eb908e5d7052a6391d706abeed4a
seoyoung park <ongddree@gmail.com>
2022-01-09 01:48:19 +0900

    :fire: 불필요한 경로 삭제

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,36 +1,26 @@
-export const API_ENDPOINT = "https://api.sampleapis.com/futurama/";
+export const API_ENDPOINT = 'https://api.sampleapis.com/futurama/';
 
-export const SEARCH_ENDPOINT = "https://www.youtube.com/results?search_query=";
+export const SEARCH_ENDPOINT = 'https://www.youtube.com/results?search_query=';
 
 export const ROUTES = [
   {
     ID: 0,
-    PATH: "/info",
-    LABEL: "info",
+    PATH: '/characters',
+    LABEL: 'characters',
   },
   {
     ID: 1,
-    PATH: "/characters",
-    LABEL: "characters",
+    PATH: '/episodes',
+    LABEL: 'episodes',
   },
   {
     ID: 2,
-    PATH: "/cast",
-    LABEL: "cast",
+    PATH: '/questions',
+    LABEL: 'questions',
   },
   {
     ID: 3,
-    PATH: "/episodes",
-    LABEL: "episodes",
-  },
-  {
-    ID: 4,
-    PATH: "/questions",
-    LABEL: "questions",
-  },
-  {
-    ID: 5,
-    PATH: "/inventory",
-    LABEL: "inventory",
+    PATH: '/inventory',
+    LABEL: 'inventory',
   },
 ];
